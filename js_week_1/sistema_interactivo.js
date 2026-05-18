07f14c43ae28cee9fce608d2f1f8c63a15ea3a9e
Adrian44-Ad <camilobpedroza@gmail.com>
2026-05-18 17:17:53 -0500

    Updating JS_WEEK

---
+++ js_week_1/sistema_interactivo.js
@@ -0,0 +1,25 @@
+
+function pressButton() {
+
+  let name = prompt("Input Your Name...");
+  let age = prompt("Input Your Age..");
+
+  // Verificacion para la var AGE
+  if (isNaN(age)) {
+    console.error("Error: Por favor, ingresa una edad válida en números.");
+    console.log("Valor invalido: " + age);
+  }
+  // Se ve que valor se ingresó
+  if (age) {
+    console.log("edad: " + age);
+  }
+
+  // Verificacion para la edead.
+  if (age < 18) {
+    alert("Hola " + name + " eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!");
+  }
+  else if (age >= 18) {
+    alert("Hola " + name + " eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!");
+  }
+}
+
